8e16118b2fcfe4b2e66b03cb3672144b5249709f
wzj <464545949@qq.com>
2025-04-03 20:50:01 +0800

    use opencv instead of PIL to save image

--- raisimGymTorch/raisimGymTorch/env/hardware/sam/sam_predict.py
+++ raisimGymTorch/raisimGymTorch/env/hardware/sam/sam_predict.py
@@ -10,8 +10,6 @@ from segment_anything import SamAutomaticMaskGenerator, sam_model_registry, SamP
 
 import matplotlib.pyplot as plt
 import os
-from typing import Any, Dict, List
-import pyrealsense2 as rs
 import numpy as np
 
 class sam_predict:
@@ -21,39 +19,13 @@ class sam_predict:
         _ = sam.to(device="cuda")
         self.generator = SamPredictor(sam)
         self.log = True
-    
-    # 分割相关
-    def show_mask(self, mask, ax, random_color=False):
-        if random_color:
-            color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
-        else:
-            color = np.array([30/255, 144/255, 255/255, 0.6])
-        h, w = mask.shape[-2:]
-        mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
-        ax.imshow(mask_image)
-    
-    # 仅显示点相关   
-    def show_points(self, coords, labels, ax, marker_size=375):
-        pos_points = coords[labels==1]
-        neg_points = coords[labels==0]
-        ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
-        ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)   
-        
-    #仅显示框相关
-    def show_box(self, box, ax):
-        x0, y0 = box[0], box[1]
-        w, h = box[2] - box[0], box[3] - box[1]
-        ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2))   
 
     def calculate_mask(self, in_img, input_point, input_label, input_box = None):
-        in_img = cv2.cvtColor(in_img, cv2.COLOR_BGR2RGB)
     
         if self.log is True:
-            plt.figure(figsize=(10,10))
-            plt.imshow(in_img)
-            self.show_points(input_point, input_label, plt.gca())
-            plt.axis('on')
-            plt.show() 
+            cv2.circle(in_img, input_point[0], 10, (0,255,0), -1)
+            cv2.imshow('Image with Circle', in_img)
+            cv2.imwrite("/home/ubuntu/hand/github/vision_dex/raisimGymTorch/raisimGymTorch/env/hardware/test/vlm_planning/test2.png", in_img)
         
         self.generator.set_image(in_img)
         masks, scores, logits = self.generator.predict(
@@ -66,14 +38,13 @@ class sam_predict:
         # method to find the best result (IOU , area% , size)
         if self.log is True:
             for i, (mask, score) in enumerate(zip(masks, scores)):
-                plt.figure(figsize=(10,10))
-                plt.imshow(in_img)
-                self.show_mask(mask, plt.gca())
-                self.show_points(input_point, input_label, plt.gca())
-                self.show_box(input_box, plt.gca())
-                plt.title(f"Mask {i+1}, Score: {score:.3f}", fontsize=18)
-                plt.axis('off')
-                plt.show()
+                cv2.rectangle(in_img, (input_box[0], input_box[1]),  (input_box[2], input_box[3]), (0,0,255), 2)
+                blue_layer = np.zeros_like(in_img)
+                blue_layer[:] = (230, 216, 173)
+                combined_image = np.where(mask[:, :, None] != 0, blue_layer, in_img)
+                cv2.circle(combined_image, input_point[0], 10, (0,255,0), -1)
+                cv2.imshow('Image with Circle 2', combined_image)
+                cv2.imwrite("/home/ubuntu/hand/github/vision_dex/raisimGymTorch/raisimGymTorch/env/hardware/test/vlm_planning/test3.png", combined_image)
 
         return masks[0]
 
